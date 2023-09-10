70b07ca9602af57b402356641025af002392a5ad
Jack <125857863+Singhsawan@users.noreply.github.com>
2023-09-10 20:41:06 +0530

    Update info.py

--- info.py
+++ info.py
@@ -25,6 +25,8 @@ PICS = (environ.get('PICS', 'https://telegra.ph/file/7215c7831b4ae88897448.jpg h
 NOR_IMG = environ.get("NOR_IMG", "https://te.legra.ph/file/a27dc8fe434e6b846b0f8.jpg")
 MELCOW_VID = environ.get("MELCOW_VID", "https://te.legra.ph/file/6f55d902f9bf2d0afd4bb.mp4")
 SPELL_IMG = environ.get("SPELL_IMG", "https://te.legra.ph/file/15c1ad448dfe472a5cbb8.jpg")
+VRFIED_IMG = environ.get("VRFIED_IMG", "https://graph.org/file/9cea98695ef1343e4f627.jpg")
+VRFY_IMG = environ.get("VRFY_IMG", "https://graph.org/file/10f9dac6eab3247e35831.jpg")
 
 # Admins, Channels & Users
 ADMINS = [int(admin) if id_pattern.search(admin) else admin for admin in environ.get('ADMINS', '5942451217').split()]
